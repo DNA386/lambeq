939d8d6b38a9a2d6af8529206ea59eab8a8fbacc
tiffany.duneau <tiffany@incubiq.com>
2025-09-26 14:22:33 +0100

    deubg mypy

--- lambeq/training/nelder_mead_optimizer.py
+++ lambeq/training/nelder_mead_optimizer.py
@@ -259,7 +259,7 @@ class NelderMeadOptimizer(Optimizer):
                 raise ValueError(
                     'Objective function must return a scalar'
                 ) from e
-        return result   # type: ignore[return-value]
+        return result
 
     def backward(self, batch: tuple[Iterable[Any], np.ndarray]) -> float:
         """Calculate the gradients of the loss function.
